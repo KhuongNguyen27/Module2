460d7ad775f060db45fa1bef052e4a02b1c48031
nguyenhuukhuong27102000@gmail.com <nguyenhuukhuong27102000@gmail.com>
2023-04-17 08:49:03 +0700

    module2

---
+++ PP2.2.5/PP2.2.5.5/index.php
@@ -0,0 +1,30 @@
+<?php
+    $matrix = [
+        [0,1,1,2],
+        [0,5,0,0],
+        [2,0,3,3]
+    ];
+    echo '<pre>';
+    $total = 0;
+    echo 'Các khách sạn thỏa mãn yêu cầu<br>';
+    for ($i= 0; $i < count($matrix); $i++) { 
+        for ($j=0; $j < count($matrix[$i]) ; $j++) { 
+            if ($matrix[$i][$j] == 0) {
+                continue;
+            }
+            if ($i==0) {
+                $total += $matrix[$i][$j];
+                echo 'Tầng: '.$i.', Phòng: '.$j.'<br>';
+                echo 'Giá phòng: '.$matrix[$i][$j].'$<br>'; 
+            }else {
+            if ($matrix[$i-1][$j] != 0) {
+                echo 'Tầng: '.$i.', Phòng: '.$j.'<br>';
+                echo 'Giá phòng: '.$matrix[$i][$j].'$<br>'; 
+                $total += $matrix[$i][$j];
+                }
+            }
+        }
+    }
+    echo 'Tổng giá phòng thỏa mãn yêu cầu: '.$total;
+    echo '</pre>';
+?>
