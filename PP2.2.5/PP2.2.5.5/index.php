383f9df94f45163b43051aab13b6df5fed980982
nguyenhuukhuong27102000@gmail.com <nguyenhuukhuong27102000@gmail.com>
2023-04-17 08:51:46 +0700

    module2

--- PP2.2.5/PP2.2.5.5/index.php
+++ PP2.2.5/PP2.2.5.5/index.php
@@ -6,7 +6,7 @@
     ];
     echo '<pre>';
     $total = 0;
-    echo 'Các khách sạn thỏa mãn yêu cầu<br>';
+    echo 'Các khách sạn thỏa mãn yêu cầu:<br><br>';
     for ($i= 0; $i < count($matrix); $i++) { 
         for ($j=0; $j < count($matrix[$i]) ; $j++) { 
             if ($matrix[$i][$j] == 0) {
@@ -15,16 +15,16 @@
             if ($i==0) {
                 $total += $matrix[$i][$j];
                 echo 'Tầng: '.$i.', Phòng: '.$j.'<br>';
-                echo 'Giá phòng: '.$matrix[$i][$j].'$<br>'; 
+                echo 'Giá phòng: '.$matrix[$i][$j].'$<br><br>'; 
             }else {
             if ($matrix[$i-1][$j] != 0) {
                 echo 'Tầng: '.$i.', Phòng: '.$j.'<br>';
-                echo 'Giá phòng: '.$matrix[$i][$j].'$<br>'; 
+                echo 'Giá phòng: '.$matrix[$i][$j].'$<br><br>'; 
                 $total += $matrix[$i][$j];
                 }
             }
         }
     }
-    echo 'Tổng giá phòng thỏa mãn yêu cầu: '.$total;
+    echo 'Tổng giá phòng thỏa mãn yêu cầu: '.$total.'$<br>';
     echo '</pre>';
 ?>
