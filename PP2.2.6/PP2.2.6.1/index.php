4d878698a5a7564a92f3d9acf1539804ff394de6
nguyenhuukhuong27102000@gmail.com <nguyenhuukhuong27102000@gmail.com>
2023-04-17 16:36:43 +0700

    module2

--- PP2.2.6/PP2.2.6.1/index.php
+++ PP2.2.6/PP2.2.6.1/index.php
@@ -3,14 +3,28 @@
     function selectionSort($list){
         // sort($list);
         // return $list;
-        for ($i=0; $i <count($list) ; $i++) { 
-            for ($j=count($list)-1; $j > $i; $j--) { 
-                if ($list[$j-1] > $list[$j]) {
-                    $temp = $list[$j];
-                    $list[$j] = $list[$j-1];
-                    $list[$j-1] = $temp;
+        // for ($i=0; $i <count($list) ; $i++) { 
+        //     for ($j=count($list)-1; $j > $i; $j--) { 
+        //         if ($list[$j-1] > $list[$j]) {
+        //             $temp = $list[$j];
+        //             $list[$j] = $list[$j-1];
+        //             $list[$j-1] = $temp;
+        //         }
+        //     }
+        // }
+        // return $list;
+        for ($i=0; $i <count($list) ; $i++) {
+            $min = $list[$i];
+            $pos = $i;
+            for ($j=$i; $j < count($list) ; $j++) { 
+                if ($min > $list[$j]) {
+                    $min = $list[$j];
+                    $pos = $j;
                 }
             }
+            $temp = $list[$i];
+            $list[$i] = $list[$pos];
+            $list[$pos] = $temp;
         }
         return $list;
     }
