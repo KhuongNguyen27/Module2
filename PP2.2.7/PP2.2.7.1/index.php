19978734d03a79a1f7a1d82b064df5ebada80d01
nguyenhuukhuong27102000@gmail.com <nguyenhuukhuong27102000@gmail.com>
2023-04-22 17:17:50 +0700

    module2

--- PP2.2.7/PP2.2.7.1/index.php
+++ PP2.2.7/PP2.2.7.1/index.php
@@ -1,14 +1,21 @@
 <?php
+    class divideZero extends Exception
+    {
+        public function _divide0(){
+            return "Không thể chia 0";
+        }
+    }
+    
     function divide($first,$second){
         if ($second === 0) {
-            throw new Exception("Không thể chia 0");
+            throw new divideZero();
         }
         return $first/$second;
     }
     try {
         echo divide(100,5)."<br>";
         echo divide(100,0)."<br>";
-    } catch (Exception $e) {
-        echo $e->getMessage();
+    } catch (divideZero $e) {
+        echo $e->_divide0();
     }
 ?>
